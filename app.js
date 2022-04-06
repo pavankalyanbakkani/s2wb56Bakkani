0857166f208a9a8d7116edafe8a77a125c9b8f6a
pavankalyanbakkani <98134544+pavankalyanbakkani@users.noreply.github.com>
2022-04-06 15:21:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dominosRouter = require('./routes/dominos');
+var addmodssRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
 
 var app = express();
