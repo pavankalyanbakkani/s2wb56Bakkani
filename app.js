0cef0c394ac1d20d6c39b5943dc11dc722515ea2
pavankalyanbakkani <98134544+pavankalyanbakkani@users.noreply.github.com>
2022-04-06 15:28:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var icemagicRouter = require('./routes/icemagic');
+var dominosRouter = require('./routes/dominos');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/icemagic', icemagicRouter);
+app.use('/dominos', dominosRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
 // catch 404 and forward to error handler
