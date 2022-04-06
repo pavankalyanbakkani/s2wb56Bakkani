9f38ae799a92bd89d17b407be9b247fa696fe84d
pavankalyanbakkani <98134544+pavankalyanbakkani@users.noreply.github.com>
2022-04-06 14:50:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dominosRouter = require('./routes/dominos');
-var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -25,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dominos', dominosRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 
 // catch 404 and forward to error handler
